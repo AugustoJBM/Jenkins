082595b6888051436329d761896042072903f745
AugustoJBM <augustojbm19@gmail.com>
2026-01-12 03:14:01 -0300

    Teste de bloqueio do Quality Gate

--- Parte 2/projeto-java/Elements/src/main/java/br/com/dbc/vemser/jedimasters/JediMasters17Application.java
+++ Parte 2/projeto-java/Elements/src/main/java/br/com/dbc/vemser/jedimasters/JediMasters17Application.java
@@ -8,5 +8,8 @@ public class JediMasters17Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JediMasters17Application.class, args);
+				// O Sonar deteta isto como credencial exposta (Hardcoded password)
+		String dbPassword = "root_password_12345";
+		String connectionUrl = "jdbc:mysql://localhost:3306/db?user=admin&password=secret_password";
 	}
 }
